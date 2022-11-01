48b31602279c5f84ad33fd9280f4cf4f459c3132
Brady Amerson <bamerson01@gmail.com>
2022-11-01 03:24:47 -0600

    L01 completed

---
+++ lesson-01/favoritecolor.py
@@ -0,0 +1,6 @@
+name = input('Hi! My name is Brady, what\'s yours? ')
+
+color = input('Nice to meet you, ' + name.capitalize() +
+              '. What is your favorite color? ')
+print('What a coincidence, ' + name.capitalize() +
+      '! I like ' + color.lower()+' too!')
