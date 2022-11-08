ba87cd932fadf7f07548636a9b0ee990bdf80911
Brady Amerson <bamerson01@gmail.com>
2022-11-08 16:11:26 -0700

    updated

--- lesson-03/areas-of-shapes.py
+++ lesson-03/areas-of-shapes.py
@@ -13,24 +13,24 @@
 import math
 
 # Area of a square - prompts user for length of a side, then multiplies it by itself, then outputs the solution
-square_length = input('What is the length of a side of the square? ')
-square_area = int(square_length)**2
-square_cubed = int(square_length)**3
+square_length = float(input('What is the length of a side of the square? '))
+square_area = square_length**2
+square_cubed = square_length**3
 square_response = f'The area of the square is: {square_area}\nThe cubic volume of the square is: {square_cubed}'
 print(square_response)
 print()
 
 # Area of a rectangle - prompts user for the length and width of the rectangle, multiplies the length and width, then outputs solution
-rectangle_length = input('What is the length of rectangle? ')
-rectangle_width = input('What is the width of the rectangle?')
-rectangle_area = int(rectangle_length)*int(rectangle_width)
+rectangle_length = float(input('What is the length of rectangle? '))
+rectangle_width = float(input('What is the width of the rectangle?'))
+rectangle_area = rectangle_length*rectangle_width
 rectangle_response = f'The area of the rectangle is: {rectangle_area}'
 print(rectangle_response)
 print()
 
 # Area of a circle - prompts user for the radius of the circle, multiplies radius by pi 3.14, outputs solution
-circle_radius = input('What is the radius of the circle? ')
-circle_area = float(circle_radius)*math.pi
+circle_radius = float(input('What is the radius of the circle? '))
+circle_area = (circle_radius ** 2) * math.pi
 circle_response = f'The area of the circle is: {circle_area}'
 print(circle_response)
 
