daf93a532203982658041467adebd3e3bbdb59d1
Brady Amerson <bamerson01@gmail.com>
2022-12-07 00:06:53 -0700

    finished shopping cart program

--- lesson-09/shopping-cart.py
+++ lesson-09/shopping-cart.py
@@ -1,47 +1,95 @@
 # This program stores a list of products in a shopping cart along with their prices. The user will have the ability to add items to the list, remove them, and see the total price of the cart.
 
+add_item = ''
+remove_item = ''
+quit = ''
+choice = None
 
-options_menu = ['Add Item', 'View Cart',
-                'Remove Item', 'Compute Total', 'Quit']
+options_menu = ['Add Item', 'View Cart (empty)', 'Remove Item', 'Cart Total', 'Quit']
 items_menu = ['hat', 'shirt', 'sweater', 'pants', 'shorts', 'socks', 'shoes']
 items_cart = []
 items_price = []
 
-add_item = ''
-view_cart = ''
-remove_item = ''
-sum_total = ''
-quit = ''
-choice = None
-continue_adding = 'yes'
-
-print()
-print('Welcome to the shopping cart program!')
-print()
+# Displays welcome message and options menu. 
+print('\nWelcome to the shopping cart program!')
 while choice != '5':
-    print('Please select one of the following:')
+    if len(items_cart) >= 1:
+        options_menu[1] = f'View Cart ({len(items_cart)} items)'
+    elif len(items_cart) < 1:
+        options_menu[1] = f'View Cart (empty)'
+    print(f'\nPlease select one of the following:')
     for i, option in enumerate(options_menu):
         print(f'{i+1}. {option}')
-    choice = input('Please enter the number: ')
-    print()
+    choice = input(f'Please enter the number: ')
 
-    if choice == '1':  # Allows user to add items to cart.
+# Allows user to add item and it's price to thier cart. item and privce are stored in seperate lists.
+    if choice == '1':
+        continue_adding = 'yes'
         while continue_adding.lower() != 'no':
-            print('Here are the items for sale:')
-            for i, item in enumerate(items_menu):  # Displays product listing.
+            # Displays product listing.
+            print(f'\nHere are the items for sale:')
+            for i, item in enumerate(items_menu):
                 print(f'{item.capitalize()}')
-            print()
-            add_item = input('What item would you like to add? ')
-            items_cart.append(add_item)
-            continue_adding = input(
-                'Would you like to continue adding items to the cart?(yes/no) ')
-    elif choice == '2':  # Allows user to view thier cart.
-        print('The contents of your cart are:')
-        for item in items_cart:
-            print(item)
-    # elif choice == 3 # Allows user to remove item from cart.
-    # elif choice == 4 # Allows user to compute total.
+            # Request user input on product to add
+            add_item = input(f'\nWhat item would you like to add? ')
+            # Validate that input is an offering from our list
+            for item in items_menu:
+                if  item.lower() == add_item.lower():             
+                    items_cart.append(add_item)
+                    item_price = float(input(f'What is the price of the {add_item}? $'))
+                    items_price.append(item_price)
+                    print(f'\n{add_item.capitalize()} has been added to the cart.')
+                    continue_adding = input(f'Continue adding items?(yes/no) ')
+                #else:
+                #    print(f'\nSorry, we don\'t offer that. Please select an item from our product list.')
+                 #   break
+# Allows user to view thier cart.
+    elif choice == '2':
+        # Validates if cart is empty and alerts user
+        if len(items_cart) < 1:
+            print(f'\nYour cart is empty.')
+        else:
+            print(f'\nThe contents of your cart are:')
+            for i, item in enumerate(items_cart):
+                item = items_cart[i]
+                item_price = items_price[i]
+                print(f'{i+1}. {item.capitalize()} - ${item_price:.2f}')
+
+# Allows user to remove item from cart.
+    elif choice == '3':
+        # Validate if there are items in cart if empty alert cart is empty and return to main options
+        if len(items_cart) < 1:
+            print(f'\nYour cart is empty. There are no items to remove.')
+        else:
+            # Display items in cart to select
+            print(f'\nWhich item would you like to remove?')
+            for i, item in enumerate(items_cart):
+                item = items_cart[i]
+                item_price = items_price[i]
+                print(f'{i+1}. {item.capitalize()} - {item_price:.2f}')
+            in_range = False
+            while in_range == False:
+                # Prompt user to select which index number to remove
+                remove_item = int(input(f'\nType the number to remove: '))
+                # Validate that user did not input anything outside the index range of the list
+                if remove_item - 1 >= 0 and remove_item - 1 <= len(items_cart) - 1:
+                    in_range = True
+                    items_cart.pop(remove_item-1)
+                    items_price.pop(remove_item-1)
+                    print('Item removed.')
+                else:
+                    print('Sorry that is an valid item number. Please try again.')
+
+# Allows user to compute total.
+    elif choice == '4':
+        sum_total = 0
+        for price in items_price:
+            sum_total += price
+        print(f'\nYour shopping cart total is ${sum_total:.2f}')
+
+# Allows user to quit the application
     elif choice == '5':
-        print('Thank you, goodbye!')
+        print(f'\nThank you, goodbye!')
+# Displays if invalid input
     else:
-        print('Not a valid option, please enter the number of an option')
+        print(f'\nNot a valid option, please enter a number of an option')
--- lesson-10/multiple-lists.py
+++ lesson-10/multiple-lists.py
@@ -3,6 +3,7 @@ balances = []
 account = ''
 balance = ''
 
+# Allows user to add accounts and balance ammount until user types quit. Stores account name and balance in seperate lists
 print('Enter the names and balances of bank accounts (type: quit when done)')
 while account.lower() != 'quit':
     account = input('What is the name of this account? ')
@@ -11,33 +12,48 @@ while account.lower() != 'quit':
         balance = float(input('What is the balance? '))
         balances.append(balance)
 
-print('Account Information:')
+# Displays account names and balance ammount for each account that was entered by user
+print('\nAccount Information:')
 for i, account in enumerate(accounts):
     account = accounts[i]
     balance = balances[i]
-    print(f'{i}.{account.capitalize()}: ${balance:.2f}')
+    print(f'{i}. {account.capitalize()}: ${balance:.2f}')
 
+# Computes the balance of all accounts and display total.
 sum_total = 0
 for balance in balances:
     sum_total += balance
-
 print(f'The total balance is: ${sum_total:.2f}')
 
+# Computes the average balance for all accounts and displays info
 average = sum_total / len(balances)
-
 print(f'The average balance is: {average:.2f}')
 
-largest_balance = 0
+# Evaluates which account has the largest balance and then displays account name and balance
+highest_balance = -999
+highest_account = None
 for i, balance in enumerate(balances):
     balance = balances[i]
     account = accounts[i]
-    if balance > largest_balance:
-        largest_balance = balance
-    print(f'Highest balance: {account}: {largest_balance}')
+    if balance > highest_balance:
+        highest_balance = balance
+        highest_account = account
+print(f'Highest balance: {account}: {highest_balance}')
 
-
-update_balance = input('Do you want to update an account balance?(yes/no) ')
+# Allows user to make updates to account balances
+update_balance = 'yes'
 while update_balance.lower() != 'no':
-    what_account = input('What account index do you want to update? ')
-    new_balance = float(input('What is the new amount? '))
-    balances[what_account] = new_balance
+    update_balance = input(
+        '\nDo you want to update an account balance?(yes/no) ')
+    if update_balance == 'yes':
+        what_account = int(input('What account index do you want to update? '))
+        new_balance = float(input('What is the new amount? '))
+        balances[what_account] = new_balance
+        # Displays account names and balance ammount for each account that was entered by user
+        print('\nAccount Information:')
+        for i, account in enumerate(accounts):
+            account = accounts[i]
+            balance = balances[i]
+            print(f'{i}. {account.capitalize()}: ${balance:.2f}')
+    else:
+        print('Have a great day!')
